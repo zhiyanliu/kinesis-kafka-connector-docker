0bb383d80f6eb2e99a8c77a82c9d2ee9b98ac497
Zhi Yan Liu <liuzhiya@amazon.com>
2020-05-28 00:44:46 +0800

    Lightly revise sed expression in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,21 +30,21 @@ else
         STATUS_TOPIC=connect-status
     fi
 
-    sed -i '/^group.id=.*/d' /worker.properties
+    sed -i '/^group\.id=.*/d' /worker.properties
     echo "group.id=${GROUP_ID}" >> /worker.properties
-    sed -i '/^offset.storage.topic=.*/d' /worker.properties
+    sed -i '/^offset\.storage\.topic=.*/d' /worker.properties
     echo "offset.storage.topic=${OFFSET_TOPIC}" >> /worker.properties
-    sed -i '/^config.storage.topic=.*/d' /worker.properties
+    sed -i '/^config\.storage\.topic=.*/d' /worker.properties
     echo "config.storage.topic=${CONFIG_TOPIC}" >> /worker.properties
-    sed -i '/^status.storage.topic=.*/d' /worker.properties
+    sed -i '/^status\.storage\.topic=.*/d' /worker.properties
     echo "status.storage.topic=${STATUS_TOPIC}" >> /worker.properties
 fi
 
 if ! [ -z $BOOTSTRAP_SERVERS ]; then
-    sed -i "/^bootstrap.servers=.*/c\bootstrap.servers=${BOOTSTRAP_SERVERS}" /worker.properties
+    sed -i "/^bootstrap\.servers=.*/c\bootstrap.servers=${BOOTSTRAP_SERVERS}" /worker.properties
 fi
 
-sed -i '/^rest.host.name=.*/d' /worker.properties
+sed -i '/^rest\.host\.name=.*/d' /worker.properties
 echo "rest.host.name=0.0.0.0" >> /worker.properties
 
 
@@ -54,19 +54,19 @@ if ! [ -z $CONNECTOR_NAME ]; then
     sed -i "/^name=.*/c\name=${CONNECTOR_NAME}" /kinesis-streams-kafka-connector.properties
 fi
 if ! [ -z $REGION ]; then
-    sed -i "/region=.*/c\region=${REGION}" /kinesis-streams-kafka-connector.properties
+    sed -i "/^region=.*/c\region=${REGION}" /kinesis-streams-kafka-connector.properties
 fi
 if ! [ -z $KINESIS_STREAM ]; then
-    sed -i "/streamName=.*/c\streamName=${KINESIS_STREAM}" /kinesis-streams-kafka-connector.properties
+    sed -i "/^streamName=.*/c\streamName=${KINESIS_STREAM}" /kinesis-streams-kafka-connector.properties
 fi
 if ! [ -z $KAFKA_TOPICS ]; then
-    sed -i "/topics=.*/c\topics=${KAFKA_TOPICS}" /kinesis-streams-kafka-connector.properties
+    sed -i "/^topics=.*/c\topics=${KAFKA_TOPICS}" /kinesis-streams-kafka-connector.properties
 fi
 if ! [ -z $MAX_TASKS ]; then
-    sed -i "/tasks.max=.*/c\tasks.max=${MAX_TASKS}" /kinesis-streams-kafka-connector.properties
+    sed -i "/^tasks\.max=.*/c\tasks.max=${MAX_TASKS}" /kinesis-streams-kafka-connector.properties
 fi
 if ! [ -z $MAX_CONNECTIONS ]; then
-    sed -i "/maxConnections=.*/c\maxConnections=${MAX_CONNECTIONS}" /kinesis-streams-kafka-connector.properties
+    sed -i "/^maxConnections=.*/c\maxConnections=${MAX_CONNECTIONS}" /kinesis-streams-kafka-connector.properties
 fi
 
 echo "worker.properties file content:"
